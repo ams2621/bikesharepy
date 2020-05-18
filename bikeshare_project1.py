4c0f41313f01a83fffe2510f8087d8aab51793b1
HVM <vardhanharsh276@gmail.com>
2020-05-18 21:23:00 +0530

    modified a statement

--- bikeshare_project1.py
+++ bikeshare_project1.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
 
-    invalid_input = "Invalid input, try again"
+    invalid_input = "Please enter a valid input.Otherwise get lost"
 
     print('Hello! Let\'s explore some US bikeshare data!')
     # TO DO: get user raw_input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
